69a85fcdbf4a87e64898c642fbeb778be008c9fc
beepbeat <zfireman885@yahoo.de>
2014-12-18 20:20:45 +0100

    Added Schwer. Complete

--- src/com/BWINF2014/Bruchrechnen/Main.java
+++ src/com/BWINF2014/Bruchrechnen/Main.java
@@ -19,7 +19,7 @@ public class Main {
         Scanner s = new Scanner(System.in);
         System.out.println("Leicht(1), Mittel(2) oder Schwer(3)?");
         switch (s.nextInt()) {
-            /*TODO Add Schwer*/
+
 
             //region Leicht
             case 1: {
@@ -70,10 +70,6 @@ public class Main {
 
             }
 //endregion
-/*
- */
-
-
 
             //region Mittel
             case 2: {
@@ -113,6 +109,55 @@ public class Main {
                             q = b / bTmp;
 
                             if ((MathUtil.Count(p) + MathUtil.Count(q)) == 5 && ( (p+q) > 10) && (p+q <= 20)) {
+                                found = true;
+                            }
+                        }
+
+
+                    }
+                }
+
+            }
+//endregion
+
+            //region Schwer
+            case 3: {
+                boolean found;
+                System.out.println("Bitte Anzahl der Aufgaben eingeben");
+                for (int i = 1; i == s.nextInt(); i++) {
+                    found = false;
+                    while (!found) {
+
+                        a = (int) MathUtil.Random(99, false);
+                        b = (int) MathUtil.Random(99, false);
+
+                        if (a < b) {
+                            int aTmp = a;
+                            while (!(b % aTmp == 0)) {
+                                aTmp--;
+                            }
+
+                            p = a / aTmp;
+                            q = b / aTmp;
+
+                            if ((MathUtil.Count(p) + MathUtil.Count(q)) == 5 && ( (p+q) > 20) && (p+q <= 30)) {
+                                found = true;
+                                Aufgaben[i] = (a + "/" + b);
+                                Loesungen[i] = (p + "/" + q);
+
+
+                            }
+
+                        } else {
+                            int bTmp = b;
+                            while (!(a % bTmp == 0)) {
+                                bTmp--;
+                            }
+
+                            p = a / bTmp;
+                            q = b / bTmp;
+
+                            if ((MathUtil.Count(p) + MathUtil.Count(q)) == 5 && ( (p+q) > 20) && (p+q <= 30)) {
                                 found = true;
                             }
                         }
