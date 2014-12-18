c08e55ae77834432f9a916a419c9c3e88a6aa8af
beepbeat <zfireman885@yahoo.de>
2014-12-18 20:17:20 +0100

    Added Mittel

--- src/com/BWINF2014/Bruchrechnen/Main.java
+++ src/com/BWINF2014/Bruchrechnen/Main.java
@@ -19,7 +19,7 @@ public class Main {
         Scanner s = new Scanner(System.in);
         System.out.println("Leicht(1), Mittel(2) oder Schwer(3)?");
         switch (s.nextInt()) {
-            /*TODO Add Mittel und Schwer*/
+            /*TODO Add Schwer*/
 
             //region Leicht
             case 1: {
@@ -30,8 +30,8 @@ public class Main {
                     while (!found) {
                        /*random[1] = (int) (Math.random() * 98 + 1);
                        random[2] = (int) (Math.random() * 98 + 1);*/
-                        a = (int) MathUtil.Random(99,false);
-                        b = (int) MathUtil.Random(99,false);
+                        a = (int) MathUtil.Random(99, false);
+                        b = (int) MathUtil.Random(99, false);
                         if (a < b) {
                             int aTmp = a;
                             while (!(b % aTmp == 0)) {
@@ -43,7 +43,7 @@ public class Main {
                             //Unused int pCount;
 
 
-                            if ((MathUtil.Count(p) + MathUtil.Count(q)) <= 10) {
+                            if ((MathUtil.Count(p) + MathUtil.Count(q)) == 4 && (p+q) <= 10) {
                                 found = true;
                                 Aufgaben[i] = (a + "/" + b);
                                 Loesungen[i] = (p + "/" + q);
@@ -70,7 +70,54 @@ public class Main {
 
             }
 //endregion
-        }
-    }
+            //region Mittel
+            case 2: {
+                boolean found;
+                System.out.println("Bitte Anzahl der Aufgaben eingeben");
+                for (int i = 1; i == s.nextInt(); i++) {
+                    found = false;
+                    while (!found) {
 
+                        a = (int) MathUtil.Random(99, false);
+                        b = (int) MathUtil.Random(99, false);
+
+                        if (a < b) {
+                            int aTmp = a;
+                            while (!(b % aTmp == 0)) {
+                                aTmp--;
+                            }
+
+                            p = a / aTmp;
+                            q = b / aTmp;
+
+                            if ((MathUtil.Count(p) + MathUtil.Count(q)) == 5 && ( (p+q) > 10) && (p+q <= 20)) {
+                                found = true;
+                                Aufgaben[i] = (a + "/" + b);
+                                Loesungen[i] = (p + "/" + q);
+
+
+                            }
+
+                        } else {
+                            int bTmp = b;
+                            while (!(a % bTmp == 0)) {
+                                bTmp--;
+                            }
+
+                            p = a / bTmp;
+                            q = b / bTmp;
+
+                            if ((MathUtil.Count(p) + MathUtil.Count(q)) == 5 && ( (p+q) > 10) && (p+q <= 20)) {
+                                found = true;
+                            }
+                        }
+
+
+                    }
+                }
+                //endregion
+            }
+        }
+
+    }
 }
